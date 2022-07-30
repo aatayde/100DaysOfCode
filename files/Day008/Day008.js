24f493297639929cc2694f61c9285b858c17d2e8
Haribo <haribo@Haribos-MBP.attlocal.net>
2022-07-29 21:21:35 -0700

    Day 8 update

--- files/Day008/Day008.js
+++ files/Day008/Day008.js
@@ -1,1 +1,35 @@
-console.log("Hello, World")
+const title = document.getElementById('title');
+const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+// console.log("is dark mode?", isDarkMode.matches)
+changeText(isDarkMode)
+// Add event listener that checks for changes in color prefers
+
+
+// Check if dark mode is ture
+// if (isDarkMode) {
+//     title.innerText = "Dark Mode"
+// } else {
+//     title.innerText = "Light Mode"
+// }
+
+// ternary shorthand | conditional shorthand
+function changeText(e){
+    return (e.matches ? title.innerText = "Dark Mode" : title.innerText = "Light Mode")
+}
+
+
+// (function (e) {
+//     return e.matches
+// })
+
+// addEventListener(isDarkMode, changeText())
+// isDarkMode.addEventListener(e => e.matches && changeText())
+// window.matchMedia("(prefers-color-scheme: dark)").addListener(
+//     e => e.matches && activateDarkMode() // listener
+//   );
+
+// IT WORKED
+isDarkMode.addEventListener("change", e => {
+    changeText(e)
+})
