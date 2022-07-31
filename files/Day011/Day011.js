461bd9e35cf2bdd5eb91817ca9506c29bd5228ee
Haribo <haribo@Haribos-MBP.attlocal.net>
2022-07-31 15:34:47 -0700

    complex array

--- files/Day011/Day011.js
+++ files/Day011/Day011.js
@@ -8,3 +8,22 @@ console.log(myArray)
 console.log(myArray.sort())
 
 console.log("length of array myArray: ", myArray.length)
+
+const myCharacter = [
+    {
+        name: "Saint",
+        class: "Cleric",
+        age: 33,
+        attributes: {
+            str: 5,
+            dex: 7,
+            faith: 50,
+            int: 13,
+            luck: 1,
+        },
+        phrases: ["Ready to heal", "I'm healing you! Now, stop dying"]
+    }
+]
+
+console.log(myCharacter)
+
