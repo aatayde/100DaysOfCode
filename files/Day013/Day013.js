5e18a2b062639ab34a3d4ba12da899bae5b7f570
Haribo <haribo@Haribos-MacBook-Pro.local>
2022-08-02 11:23:54 -0700

    add reduce method to add values

--- files/Day013/Day013.js
+++ files/Day013/Day013.js
@@ -1,3 +1,9 @@
 const array = [1,2,3,4,5]
 
-console.log(array)
+// Sum values in array using Reduce method
+const initialValue = 0;
+const sumWithInitial = array.reduce(
+    (previousValue, currentValue) => previousValue + currentValue, initialValue
+)
+
+console.log("sum of values: ",sumWithInitial)
