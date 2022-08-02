304d734bab7e393b3689463626fcd67805af0025
Haribo <haribo@Haribos-MBP.attlocal.net>
2022-08-01 21:52:40 -0700

    add object

--- files/Day012/Day012.js
+++ files/Day012/Day012.js
@@ -1,1 +1,10 @@
-console.log("hello, World!")
+const object = {
+    "item-1": 1,
+    "item-2": 2,
+    "item-3": 3,
+}
+
+console.log(object)
+
+const summmed = sumOfValues(object)
+console.log(summed)
