facecf99ae1a916dba7b62171f8177ab7792babd
Hachi <alejandro.atayde.jr@gmail.com>
2022-08-15 22:29:34 -0700

    create element

--- files/Day025/Day025.js
+++ files/Day025/Day025.js
@@ -1,1 +1,18 @@
 console.log("hello world!")
+
+function addElement(){
+
+    // Create new div
+   const newDiv =  document.createElement("div")
+
+    // Give div content
+    const newContent = document.createTextNode("Hello, World!")
+
+    // Add text to new div
+    newDiv.appendChild(newContent)
+
+    // Add new div & its content into the DOM
+    const currentElement = document.getElementById('title');
+    document.body.insertBefore(newDiv, currentElement)
+}
+
