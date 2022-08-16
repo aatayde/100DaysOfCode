589f1d8d4b6efad3a1da20d2dd4ffb235bc42fe8
Hachi <alejandro.atayde.jr@gmail.com>
2022-08-15 20:37:07 -0700

    completed day 24 challenge

--- files/Day024/Day024.js
+++ files/Day024/Day024.js
@@ -1,11 +1,28 @@
 var MY_KEY = config.key
 var url = "https://api.weatherapi.com/v1/current.json?key=" + MY_KEY + "&q=seattle&aqi=no"
 
-console.log('hello, World')
-console.log(url)
 
-fetch(url)
-  
+console.log(url)
+function getWeather(){
+
+    let location = document.getElementById('location')
+    let temp = document.getElementById('temp')
+    let condition = document.getElementById('condition')
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            condition.innerText = data.current.condition.text; 
+            location.innerText = data.location.name;
+            temp.innerText = data.current.temp_f;
+        })
+        // .then((data) => console.log(data.current.condition.text))
+    
+        
+        
+}
+        
+getWeather()
 
 // location.name
 // location.region
