95c7b42bd653d5b2eb04b4b03f79d57c00dfd3b3
Haribo <haribo@Haribos-MacBook-Pro.local>
2022-08-22 12:43:43 -0700

    remove comments

--- files/Day030/script.sh
+++ files/Day030/script.sh
@@ -10,32 +10,32 @@ sleep 1
 
 # Create new brach to work out of
 echo "Create new branch"
-# git checkout -b Day-$day
+git checkout -b Day-$day
 sleep 1
 
 # create directory
 echo "Create directory"
-# mkdir Day0$day/
+mkdir Day0$day/
 sleep 1
 
 # Create markdown
 echo "Create markdown"
-# touch Day0$day/README.md 
+touch Day0$day/README.md 
 sleep 1
 
 # add staging changes
 echo "Add staging changes"
-# git add .
+git add .
 sleep 1
 
 # commit changes
 echo "Commit changes"
-# git commit -m "add folder & markdown"
+git commit -m "add folder & markdown"
 sleep 1
 
 # push changes to current branch
 echo "Push changes to current branch"
-# git push -u origin Day-$day
+git push -u origin Day-$day
 sleep 1
 
 # complete
