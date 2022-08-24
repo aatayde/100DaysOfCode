9cb071672e043acfb3d0c64e0772017106133a1c
Haribo <haribo@Haribos-MBP.attlocal.net>
2022-08-23 21:07:51 -0700

    add obejct to localStorage, log data

--- files/Day032/Day032.js
+++ files/Day032/Day032.js
@@ -1,5 +1,11 @@
+// Create object
 localStorage.setItem('myCat', 'Tom');
 
+// Save object to variable
 const cat = localStorage.getItem('myCat');
 
+// log variable
 console.log(cat)
+
+// log localStorage
+console.log(window.localStorage)
