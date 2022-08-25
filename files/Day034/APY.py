eac845ec66fb2d00f61420f7cd503d2e2c5abb68
Haribo <haribo@Haribos-MacBook-Pro.local>
2022-08-25 11:10:21 -0700

    add starting values

---
+++ files/Day034/APY.py
@@ -0,0 +1,17 @@
+# Incrimenting 5% to the amount entered until reaching desired amount
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ User Input ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# User input for savingAmount
+print('Please enter the amount you wish to add monthly:')
+investment = int(input())
+
+# user input for Percentage
+print('Please enter the percentage you wish to increase the starting amount by:')
+percentageIncrease = int(input())
+
+#user input for Time
+print('Please enter the desired amout you would like to reach:')
+time = int(input())
+
+
+
--- files/Day034/README.md
+++ files/Day034/README.md
@@ -1,1 +1,6 @@
 # Day 34
+
+## Python 
+
+    Have you ever had a bank account with an APY?
+    Try entering the amount you would like to invest, the percentage you would like to gain per Year, & the amount you would like to read. Be amazed by the results.
