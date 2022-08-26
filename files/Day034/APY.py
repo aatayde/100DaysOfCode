bf95b73c82a4fcbec9657658f43eaf9564892f8c
Haribo <haribo@Haribos-MBP.attlocal.net>
2022-08-25 21:25:53 -0700

    add 2 space decimal format

--- files/Day034/APY.py
+++ files/Day034/APY.py
@@ -7,11 +7,36 @@ investment = int(input())
 
 # user input for Percentage
 print('Please enter the percentage you wish to increase the starting amount by:')
-percentageIncrease = int(input())
+percentageIncrease = int(input()) / 100
 
 #user input for Time
 print('Please enter the desired amout you would like to reach:')
-time = int(input())
+target = float(input())
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Calculations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+savingAmount = 0
+
+# while loop
+counter = 0
+while savingAmount < target:
+
+# Add month to counter
+    counter += 1
+    print('Month: ' + str(counter))
+# Step 1, Add investment to savings
+    savingAmount += investment
+    # print('savingAmount: $' + str(savingAmount))
+    increase = ((savingAmount * percentageIncrease))
+    # print('Amount earned from percentage: $' + str(increase))
+    print('investment + intrest: $' + "{:.2f}".format(savingAmount) + ' + $' + "{:.2f}".format(increase))
+
+# Total
+    total = increase + savingAmount
+    print('Your total is: $' + "{:.2f}".format(total) + ', after ' + str(counter) + ' month of investing')
+
+    savingAmount = total
+    # print('Savings after ' + str(counter) + 'months: ' + str(savingAmount))
+
+print('number of months this until goal was reached: ' + str(counter))
