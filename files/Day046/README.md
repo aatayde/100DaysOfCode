c7a88f4b26dfec9c4c59025b4804d15b245dd770
haribo <haribo@haribos-MBP.attlocal.net>
2022-09-11 01:49:59 -0700

    update files

--- files/Day046/README.md
+++ files/Day046/README.md
@@ -1,1 +1,3 @@
 # Day 46
+
+## [PHP](https://developer.mozilla.org/en-US/docs/Glossary/PHP) Hypertext Preprocessor
---
+++ files/Day046/server.php
@@ -0,0 +1,14 @@
+<!-- start of php code -->
+<?php
+
+echo "hello world!";
+
+// Variables
+$name='Alejandro';
+$occupation='Web Developer';
+$state='California';
+
+echo "<h2>Im $name, a $occupation from $state</h2>";
+
+?>
+<!-- End of php code -->
