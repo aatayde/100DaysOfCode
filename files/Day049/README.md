4530b25a16d44a0533586fe3ec7163264dd204eb
haribo <haribo@haribos-MBP.attlocal.net>
2022-09-14 13:44:30 -0700

    move user input inside do while loop to request input after each election

--- files/Day049/README.md
+++ files/Day049/README.md
@@ -1,1 +1,7 @@
 # Day 49
+
+## C++ Switch Case Menu 
+
+### Objective
+
+    create a switch case menu that runs until you choose to quit the program.
--- files/Day049/menu.cpp
+++ files/Day049/menu.cpp
@@ -5,21 +5,23 @@ int main(){
 
 int menu;
 
-cout << "1. Print Valid Items Unsorted. \n 2. Print invalid records from an error file. \n 3. Exit program. \n";
 
-cin >> menu;
+do {
 
+    cout << "1. Print Valid Items Unsorted. \n2. Print invalid records from an error file. \n3. Exit program. \n";
+    cin >> menu;
     switch(menu){
         case 1:
-            cout << "Valid Item Records";
+            cout << "Valid Item Records \n";
             break;
         case 2:
-            cout << "Invalid Item Records";
+            cout << "Invalid Item Records \n";
             break;
         case 3:
-            cout << "Quiting program";
+            cout << "Quitting program \n";
             break;
     }
+    } while (menu != 3);
     
     return 0;
 }
