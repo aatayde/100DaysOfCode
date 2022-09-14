7b08ef3608182fca0b29a6f9ad21d98ccc897cc1
haribo <haribo@haribos-MBP.attlocal.net>
2022-09-13 22:50:33 -0700

    add variables, multiple decleration, float, bool, string, console input & output

--- files/Day048/README.md
+++ files/Day048/README.md
@@ -1,1 +1,7 @@
 # Day 48
+
+## My first C++ Program
+
+    before starting, a compiler is requred. im working with MacOS so I have to use GNU compiler collection (GCC) or clang from xCode. luckily both are already installed. I verified by using the --version command.
+
+[VS code extension](https://code.visualstudio.com/docs/languages/cpp)
--- files/Day048/myfirstprogram.cpp
+++ files/Day048/myfirstprogram.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 int main(){
-    cout << "Hello, World";
+    // My first comment!
+    cout << "Hello, World! \n";
+    cout << "I'm learning C++" << endl;
+
+    // Variables
+    const int x = 3, y = 6, z = 9;
+    float pi = 3.14;
+    char myFavoriteChar = 'X';
+    string myString = "this is a string of words";
+    bool lightSwitch = false;
+
+
+    cout << "What's your name? \n";
+    // declare variable before requesting input
+    string name;
+    // request input
+    cin >> name;
+    cout << "Hello, " << name << "! \n";
+    cout << "What's your age? \n";
+    int age;
+    cin >> age;
+    cout << "You were born in year " << 2022 - age << "!\n";
+
     return 0;
 }
