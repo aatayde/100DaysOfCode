f9dc33f9cf7447dad32a9a32fdfa131e179c9360
haribo <haribo@haribos-MacBook-Pro.local>
2022-09-22 22:01:01 -0700

    test logged values with typeof

--- files/Day058/script.js
+++ files/Day058/script.js
@@ -1,6 +1,13 @@
 console.log("Hello World!")
-const calculate = document.getElementById('calculate')
+
+const power = Number(document.getElementById("power").value);
+const storage = Number(document.getElementById("storage").value);
+
 
 function clicked(){
-    return alert("I've been clicked")
+    // This is not showing up an alert messages when clicked
+
+    console.log(typeof(storage))
+    console.log(typeof(power))
+    console.log(power+storage)
 }
