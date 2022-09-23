44acc03552fc4126a8fdfe58966715b88c30f593
haribo <haribo@haribos-MacBook-Pro.local>
2022-09-23 07:44:16 -0700

    save user input to localStorage

--- files/Day059/script.js
+++ files/Day059/script.js
@@ -1,7 +1,7 @@
 
 function save(){
-    console.log(document.getElementById("dog").value)
+    dogName = document.getElementById("dog").value
 
-    // localStorage.setItem('myDog', 'Jerry');
-    // console.log(localStorage)
+    localStorage.setItem('myDog', dogName);
+    console.log(localStorage)
 }
