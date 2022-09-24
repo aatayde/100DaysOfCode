4ed2d74ae61b79ab55009546d0cf15d051ab9f77
haribo <haribo@haribos-MBP.attlocal.net>
2022-09-24 14:40:15 -0700

    add buttons with onclick events

--- files/Day059/script.js
+++ files/Day059/script.js
@@ -1,7 +1,13 @@
 
 function save(){
     dogName = document.getElementById("dog").value
-
     localStorage.setItem('myDog', dogName);
+}
+
+function remove(){
+    localStorage.removeItem('myDog');
+}
+
+function log(){
     console.log(localStorage)
 }
