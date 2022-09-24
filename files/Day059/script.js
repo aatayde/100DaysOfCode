cdfcca7fa12b37d55fd2e27b51a7884d47a48f24
haribo <haribo@haribos-MBP.attlocal.net>
2022-09-24 15:21:13 -0700

    display localStorage in heading element

--- files/Day059/script.js
+++ files/Day059/script.js
@@ -1,13 +1,19 @@
+let heading = document.getElementById("heading")
 
 function save(){
     dogName = document.getElementById("dog").value
     localStorage.setItem('myDog', dogName);
+    log()
 }
 
 function remove(){
     localStorage.removeItem('myDog');
+    log()
 }
 
 function log(){
+    let logDog = localStorage.getItem('myDog');
+    heading.innerText = logDog;
+    
     console.log(localStorage)
 }
