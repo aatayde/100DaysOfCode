61b30618ccb5ec889880e151a8b80a631b734c04
aatayde <alejandro.atayde.jr@gmail.com>
2023-10-04 19:11:35 -0700

    select all sections with class hidden in JS

---
+++ files/Day080/app.js
@@ -0,0 +1,5 @@
+// select all section classes
+const hiddenElements = document.querySelectorAll(".hidden");
+// lets confirm we have 3 sections with the class hidden
+console.log(hiddenElements);
+// confirmed! working as intended
