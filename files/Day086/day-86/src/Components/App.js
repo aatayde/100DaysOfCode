784b86366232a2c0889b9bb3e95f823d3c60b921
aatayde <alejandro.atayde.jr@gmail.com>
2024-04-12 20:51:03 -0700

    add logic to app & styling

--- files/Day086/day-86/src/Components/App.js
+++ files/Day086/day-86/src/Components/App.js
@@ -1,10 +1,22 @@
-import "./App.css";
-import Heading from "./Components/Heading";
+import "../App.css";
+import Heading from "./Heading";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+  function handleClick() {
+    console.log("i have been clicked");
+    return setCounter(counter * 2);
+  }
   return (
     <div>
       <Heading />
+      <div className="absolute-center">
+        <span className="center-text">{counter}</span>
+        <button className="button auto-margin" onClick={handleClick}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
