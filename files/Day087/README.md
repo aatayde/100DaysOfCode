837db06e93a59d165cb069d1840ae8936edc3874
aatayde <alejandro.atayde.jr@gmail.com>
2024-05-01 14:16:23 -0700

    adding day 87 code

--- files/Day087/README.md
+++ files/Day087/README.md
@@ -2,9 +2,13 @@
 
 ## Day 87
 
-### thoughts & planning
+### Thoughts & planning
 
 - use javascript to display the timer & update it dynamically every second
+
+1. set birth date & time
+2. calculate how much time has past since that day in hours, minutes, & seconds
+3. update timer in intervals of 1 second
 
 ### Resources
 
--- files/Day087/script.js
+++ files/Day087/script.js
@@ -1,1 +1,39 @@
-console.log("javascript is loading");
+function timeSinceBirthDate() {
+  const birthday = new Date("1993-04-13T09:38:00");
+  console.log(birthday);
+
+  const now = new Date();
+  console.log(now);
+
+  // get the difference of birthday & current time
+  let difference = now - birthday;
+  console.log(difference);
+
+  // seconds
+  var seconds = Math.floor((difference / 1000) % 60);
+  console.log("Seconds", seconds);
+
+  // minutes
+  var minutes = Math.floor((difference / 1000 / 60) % 60);
+  console.log("Minutes", minutes);
+
+  // hours
+  var hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+  console.log("Hours", hours);
+
+  // days
+  var days = Math.floor((difference / (1000 * 60 * 60 * 24)) % 30);
+  console.log("Days", days);
+
+  // years
+  var years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365));
+  console.log("Year", years);
+
+  return `Years ${years} - Days ${days} - Hours ${hours} - Minutes ${minutes} - Seconds ${seconds}`;
+}
+
+function updateTime() {
+  document.getElementById("time").innerText = timeSinceBirthDate();
+}
+
+setInterval(updateTime, 1000);
