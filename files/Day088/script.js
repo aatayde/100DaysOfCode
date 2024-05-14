758cc460a4b2b1ffedb2ec9106b2e363f6b6d056
aatayde <alejandro.atayde.jr@gmail.com>
2024-05-14 13:47:33 -0700

    adding javascript logic

--- files/Day088/script.js
+++ files/Day088/script.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let output = document.getElementById("output");
+
+console.log(num1);
+console.log(num2);
+
+// add event listener
+num1.addEventListener("input", updateOutput);
+num2.addEventListener("input", updateOutput);
+
+// function to update the output field
+function updateOutput() {
+  // calculate the difference
+  let difference = parseInt(num1.value, 10) - parseInt(num2.value, 10);
+
+  if (difference > 0) {
+    // apply green text font
+    output.setAttribute("style", "color: #93E9BE");
+  } else {
+    output.setAttribute("style", "color: #D22B2B");
+  }
+  output.innerText = difference;
+}
