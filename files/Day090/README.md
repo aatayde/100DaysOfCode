14430e3be4d2e1877e599106c2db1b51cd5511c1
aatayde <alejandro.atayde.jr@gmail.com>
2024-06-05 16:48:18 -0700

    add starting files

--- files/Day090/README.md
+++ files/Day090/README.md
@@ -1,1 +1,3 @@
-# Day 90
+# Node.js
+
+## Day 90
---
+++ files/Day090/script.js
@@ -0,0 +1,24 @@
+// asynchronous operations
+
+const fs = require("fs");
+
+// blocking, synchronous way
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written!");
+
+// non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`);
+      console.log("Your file has been written 👌🏻");
+    });
+  });
+});
