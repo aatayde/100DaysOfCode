42c00adb8934055c51305e876b396ef1a83fde79
aatayde <alejandro.atayde.jr@gmail.com>
2024-06-05 21:51:57 -0700

    run server on port 8000

--- files/Day091/script.js
+++ files/Day091/script.js
@@ -6,6 +6,10 @@ const http = require("http");
 
 /////////////////////////////
 // SERVER
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
   res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
