d31ffe22ac6683d900589ae439bdcc472dd121a9
aatayde <alejandro.atayde.jr@gmail.com>
2024-06-19 19:39:34 -0700

    add time updating dynamically

--- files/Day096/script.js
+++ files/Day096/script.js
@@ -8,3 +8,22 @@ console.log(month);
 date.innerText = `${month} ${day}`;
 
 // time
+function updateClock() {
+  let currentTime = new Date();
+
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  time.innerText = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateClock, 1000);
