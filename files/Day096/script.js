cbb4ffaf43b955556a4c5b72cd2c34f8636c95a6
aatayde <alejandro.atayde.jr@gmail.com>
2024-06-19 19:20:28 -0700

    add current day & month to script

--- files/Day096/script.js
+++ files/Day096/script.js
@@ -1,1 +1,10 @@
-console.log("hello world!");
+// Date
+let date = document.getElementById("date");
+let currentDate = new Date();
+let day = currentDate.getDay();
+let month = currentDate.toLocaleString("default", { month: "long" });
+console.log(month);
+
+date.innerText = `${month} ${day}`;
+
+// time
